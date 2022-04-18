102f949c467b9aac8d20220256199e4d68682581
skill inui <k-inui@sic-net.co.jp>
2022-04-18 17:31:12 +0900

    [受注CSV][項目追加]販売タイプ

--- 1.テーブル反映/4a.[csv項目追加]受注一覧にオプション項目追加.sql
+++ 1.テーブル反映/4a.[csv項目追加]受注一覧にオプション項目追加.sql
@@ -193,3 +193,9 @@ INSERT INTO `dtb_csv` (`csv_type_id`, `creator_id`, `entity_name`, `field_name`,
 INSERT INTO `dtb_csv` (`csv_type_id`, `creator_id`, `entity_name`, `field_name`, `reference_field_name`, `disp_name`, `sort_no`, `enabled`, `create_date`, `update_date`, `discriminator_type`) VALUES
 (3, 1, 'Eccube\\\\Entity\\\\Shipping', 'shipping_shukkayotei_date', NULL, '出荷予定日', 6, 1, '2021-09-02 03:47:59', '2022-02-04 11:47:05', 'csv'),
 (4, 1, 'Eccube\\\\Entity\\\\Shipping', 'shipping_shukkayotei_date', NULL, '出荷予定日', 56, 0, '2021-09-02 03:47:59', '2022-03-27 04:19:48', 'csv');
+
+
+--販売タイプ
+INSERT INTO `dtb_csv` (`csv_type_id`, `creator_id`, `entity_name`, `field_name`, `reference_field_name`, `disp_name`, `sort_no`, `enabled`, `create_date`, `update_date`, `discriminator_type`) VALUES
+(3, 1, 'Eccube\\\\Entity\\\\Order', 'uketori_type', NULL, '販売タイプ(ECから取り置き注文：1、ECから配送注文：2、店舗で注文：空白)', 6, 1, '2021-09-02 03:47:59', '2022-02-04 11:47:05', 'csv'),
+(4, 1, 'Eccube\\\\Entity\\\\Order', 'uketori_type', NULL, '販売タイプ(ECから取り置き注文：1、ECから配送注文：2、店舗で注文：空白)', 56, 0, '2021-09-02 03:47:59', '2022-03-27 04:19:48', 'csv');
